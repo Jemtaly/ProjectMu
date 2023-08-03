5791dc07e37d840191ab3dc4fa8a9231d99f3443
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2023-08-04 03:25:53 +0800

    add error handling

--- mu.cpp
+++ mu.cpp
@@ -1,4 +1,5 @@
-#include <math.h>
+#include <cmath>
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <stack>
@@ -42,8 +43,8 @@ struct WavHead {
         riff_size(sizeof wave_ID + sizeof fmat_ID + sizeof fmat_size + sizeof fmat + sizeof data_ID + sizeof data_size + ds) {}
 };
 struct Tone {
-    double freq, dura = 0;
-    std::vector<BITS_T> wave(char t) const {
+    double freq, dura = 0.0;
+    auto wave(char t) const {
         int size = dura * WAV_SR;
         int peri = WAV_SR / freq;
         std::vector<BITS_T> wave(size, BITS_M);
@@ -60,77 +61,74 @@ struct Tone {
     }
 };
 class Passage {
+    static inline int const pitch[7] = {0, 2, 4, 5, 7, 9, 11};
     struct Note {
         Rational value;
         int solfa, accidental, octave = 0;
     };
-    struct Warn {
-        int mctr;
-        Rational mval;
-    };
     std::vector<Note> notes;
-    std::vector<Warn> warns;
-    std::string const mode;
     std::pair<int, int> const metr;
     int const bpm;
+    int reference;
 public:
-    Passage(std::string const &md, std::pair<int, int> const &mt, int tm, std::ifstream &input):
-        mode(md), metr(mt), bpm(tm) {
+    Passage(std::string const &mode, std::pair<int, int> const &metr, int bpm, std::ifstream &input):
+        metr(metr), bpm(bpm), reference(3) {
+        for (auto c : mode) {
+            switch (c) {
+            case 'C': case 'D': case 'E': case 'F': case 'G': case 'A': case 'B':
+                reference = (pitch[(c - 'C' + 7) % 7] + 3) % 12 + 0; break;
+            case '#':
+                reference++; break;
+            case 'b':
+                reference--; break;
+            case '^':
+                reference += 12; break;
+            case 'v':
+                reference -= 12; break;
+            }
+        }
         Rational crotchet(1, 4);
         std::stack<Rational> tuplets;
-        for (int mctr = 1, c; c = input.peek(), c != '&' && c != '|' && c != ':'; mctr++) {
+        for (int mctr = 1, c; c = input.peek(), c != '&' && c != '|' && c != ':' && c != '~'; mctr++) {
             int persist[7] = {0};
             std::vector<Note> measure;
             for (int as, bs, c; c = input.get(), c != '|';) {
                 switch (c) {
                 case '1': case '2': case '3': case '4': case '5': case '6': case '7':
-                    measure.push_back({crotchet, c, persist[c - '1']});
-                    break;
+                    measure.push_back({crotchet, c, persist[c - '1']}); break;
                 case ',': case '0':
-                    measure.push_back({crotchet, c, 0});
-                    break;
+                    measure.push_back({crotchet, c, 0}); break;
                 case '=':
-                    measure.back().accidental = persist[measure.back().solfa - '1'] = 0;
-                    break;
+                    measure.back().accidental = persist[measure.back().solfa - '1'] = 0; break;
                 case '#':
-                    measure.back().accidental = persist[measure.back().solfa - '1'] = 1;
-                    break;
+                    measure.back().accidental = persist[measure.back().solfa - '1'] = 1; break;
                 case 'b':
-                    measure.back().accidental = persist[measure.back().solfa - '1'] = -1;
-                    break;
+                    measure.back().accidental = persist[measure.back().solfa - '1'] = -1; break;
                 case '^':
-                    measure.back().octave++;
-                    break;
+                    measure.back().octave++; break;
                 case 'v':
-                    measure.back().octave--;
-                    break;
+                    measure.back().octave--; break;
                 case '-':
-                    measure.back().value += crotchet;
-                    break;
+                    measure.back().value += crotchet; break;
                 case '/':
-                    measure.back().value /= 2;
-                    break;
+                    measure.back().value /= 2; break;
                 case '.':
-                    measure.back().value *= Rational(1, 2 * (measure.back().value / crotchet).numerator()) + 1;
-                    break;
+                    measure.back().value *= Rational(1, 2 * (measure.back().value / crotchet).numerator()) + 1; break;
                 case '<':
-                    crotchet /= 2;
-                    break;
+                    crotchet /= 2; break;
                 case '>':
-                    crotchet *= 2;
-                    break;
+                    crotchet *= 2; break;
                 case '[':
-                    input >> as;
-                    input.get(); // ':'
-                    input >> bs;
-                    input.get(); // ']'
+                    input >> as; input.get(); // ':'
+                    input >> bs; input.get(); // ']'
                     tuplets.emplace(as, bs);
                     crotchet /= tuplets.top();
                     break;
                 case '!':
                     crotchet *= tuplets.top();
-                    tuplets.pop();
-                    break;
+                    tuplets.pop(); break;
+                case EOF:
+                    throw std::runtime_error("Unexpected end of file.");
                 }
             }
             Rational mval;
@@ -138,58 +136,22 @@ public:
                 mval += n.value;
             }
             if (mval != Rational(metr.first, metr.second)) {
-                warns.push_back({mctr, mval});
+                std::cerr << color_warn << "Warn: " << color_end
+                          << "the " << std::to_string(mctr)
+                           + "tsnrtttttt"[(mctr % 100) / 10 == 1 ? 0 : mctr % 10]
+                           + "htddhhhhhh"[(mctr % 100) / 10 == 1 ? 0 : mctr % 10] << " measure is irregular. "
+                          << "(" << std::to_string(mval.numerator()) << "/" << std::to_string(mval.denominator()) << ")" << std::endl;
             }
             notes.insert(notes.end(), measure.begin(), measure.end());
         }
     }
-    void pshow(int i = 0) const {
-        std::cerr << color_info << "Passage " << i << ": " << color_end
-                  << "mode = " << mode << ", "
-                  << "metr = " << metr.first << "/" << metr.second << ", "
-                  << "speed = " << bpm << " bpm, "
-                  << "notes = " << notes.size() << "." << std::endl;
-        for (auto const &warn : warns) {
-            std::cerr << color_warn << "Warn: " << color_end
-                      << "the " << std::to_string(warn.mctr)
-                       + "tsnrtttttt"[(warn.mctr % 100) / 10 == 1 ? 0 : warn.mctr % 10]
-                       + "htddhhhhhh"[(warn.mctr % 100) / 10 == 1 ? 0 : warn.mctr % 10] << " measure is irregular. "
-                      << "(" << std::to_string(warn.mval.numerator()) << "/" << std::to_string(warn.mval.denominator()) << ")" << std::endl;
-        }
-    }
-    std::vector<Tone> tones() const {
-        int const pitch[7] = {0, 2, 4, 5, 7, 9, 11};
-        int reference = 3;
-        if (mode[0] >= 'A' && mode[0] <= 'G') {
-            reference = (pitch[(mode[0] - 'C' + 7) % 7] + 3) % 12 + 0;
-        }
-        if (mode[0] >= 'a' && mode[0] <= 'g') {
-            reference = (pitch[(mode[0] - 'c' + 7) % 7] + 3) % 12 + 3;
-        }
-        for (auto c : mode.substr(1)) {
-            switch (c) {
-            case '#':
-                reference++;
-                break;
-            case 'b':
-                reference--;
-                break;
-            case '^':
-                reference += 12;
-                break;
-            case 'v':
-                reference -= 12;
-                break;
-            }
-        }
-        std::vector<Tone> tones;
+    auto tones() const {
+        std::vector<Tone> tones = {{0.0}};
         for (auto const &note : notes) {
             if (note.solfa != ',') {
                 tones.push_back({note.solfa != '0' ? 440 * pow(2, (reference + pitch[note.solfa - '1'] + note.accidental) / 12.0 + note.octave) : 0.0});
             }
-            if (tones.size() != 0) {
-                tones.back().dura += (note.value * 60 * metr.second / bpm).value();
-            }
+            tones.back().dura += (note.value * 60 * metr.second / bpm).value();
         }
         return tones;
     }
@@ -203,26 +165,27 @@ public:
         std::pair<int, int> metr = {4, 4};
         int bpm = 88;
         char endchar;
-        for (endchar = '&'; endchar == '&'; endchar = input.get()) {
-            if (std::string temp; input >> temp, temp.size() && temp != "~") {
-                mode = temp;
-                input >> metr.first >> endchar >> metr.second >> bpm;
+        for (endchar = '&'; endchar == '&' || endchar == '~'; endchar = input.get()) {
+            if (endchar == '&') {
+                input >> mode >> metr.first >> endchar >> metr.second >> bpm;
             }
             passages.emplace_back(mode, metr, bpm, input);
-        };
+            std::cerr << color_info << "Passage " << passages.size() << ": " << color_end
+                      << "mode = " << mode << ", "
+                      << "metr = " << metr.first << "/" << metr.second << ", "
+                      << "speed = " << bpm << " bpm." << std::endl;
+        }
         if (endchar == ':') {
             for (int i; input >> i;) {
+                if (i < 1 || i > passages.size()) {
+                    throw std::runtime_error("Passage index out of range.");
+                }
                 order.push_back(i - 1);
             }
         } else {
             for (int i = 0; i < passages.size(); i++) {
                 order.push_back(i);
             }
-        }
-    }
-    void mshow() const {
-        for (int i = 0; i < passages.size(); i++) {
-            passages[i].pshow(i + 1);
         }
         std::cerr << color_info << "Passage order: " << color_end;
         for (int i = 0; i < order.size() - 1; i++) {
@@ -253,8 +216,7 @@ int main(int argc, char *argv[]) {
 #if defined _WIN32
     HANDLE hStderr = GetStdHandle(STD_ERROR_HANDLE);
     DWORD dwStderrMode;
-    BOOL bStderr = GetConsoleMode(hStderr, &dwStderrMode);
-    color_support = bStderr && SetConsoleMode(hStderr, dwStderrMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    color_support = GetConsoleMode(hStderr, &dwStderrMode) && SetConsoleMode(hStderr, dwStderrMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 #elif defined __unix__
     color_support = isatty(fileno(stderr));
 #endif
@@ -265,9 +227,9 @@ int main(int argc, char *argv[]) {
         color_end = "\033[0m";
     }
     int rec = 0;
-    std::ifstream txt_file;
-    std::ofstream wav_file;
     char timbre = '0';
+    std::string txt_name = "-"; 
+    std::string wav_name = "a.wav";
     for (int i = 1; (rec & REC_ERR) == 0 && i < argc; i++) {
         if (argv[i][0] == '-') {
             if (argv[i][1] == 't') {
@@ -277,7 +239,8 @@ int main(int argc, char *argv[]) {
                     rec |= REC_ERR;
                 }
             } else if (argv[i][1] == 'o' && argv[i][2] == '\0') {
-                if ((rec & REC_WAV) == 0 && i + 1 < argc && (wav_file.open(argv[++i], std::ios::binary), wav_file.is_open())) {
+                if ((rec & REC_WAV) == 0 && i + 1 < argc) {
+                    wav_name = argv[++i];
                     rec |= REC_WAV;
                 } else {
                     rec |= REC_ERR;
@@ -285,13 +248,14 @@ int main(int argc, char *argv[]) {
             } else {
                 rec |= REC_ERR;
             }
-        } else if ((rec & REC_TXT) == 0 && (txt_file.open(argv[i]), txt_file.is_open())) {
+        } else if ((rec & REC_TXT) == 0) {
+            txt_name = argv[i];
             rec |= REC_TXT;
         } else {
             rec |= REC_ERR;
         }
     }
-    if ((rec & REC_ERR) != 0 || (rec & REC_TXT) == 0 || (rec & REC_WAV) == 0 && not(wav_file.open("a.wav", std::ios::binary), wav_file.is_open())) {
+    if ((rec & REC_ERR) != 0 || (rec & REC_TXT) == 0) {
         std::cerr << "Description: Generate audio file from numeric music notation." << std::endl
                   << "Usage: " << argv[0] << " [-o OUTFILE] [-t<n>] FILE" << std::endl
                   << "Options:" << std::endl
@@ -304,11 +268,20 @@ int main(int argc, char *argv[]) {
                   << "  -t4         timbre: superimposed sine waves" << std::endl;
         return 1;
     }
-    Music mu(txt_file);
-    mu.mshow();
-    mu.msave(wav_file, timbre);
-#if defined _WIN32
-    color_support = bStderr && SetConsoleMode(hStderr, dwStderrMode);
-#endif
+    try {
+        std::ifstream txt_file = std::ifstream(txt_name);
+        if (not txt_file.is_open()) {
+            throw std::runtime_error("Cannot open the input file.");
+        }
+        Music mu(txt_file);
+        std::ofstream wav_file = std::ofstream(wav_name, std::ios::binary);
+        if (not wav_file.is_open()) {
+            throw std::runtime_error("Cannot open the output file.");
+        }
+        mu.msave(wav_file, timbre);
+    } catch (std::exception const &e) {
+        std::cerr << color_err << "Error: " << color_end << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
